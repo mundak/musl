13af0f3232c3adc42f5971fa7df4877e2da86802
Marcin Undak <mcinek@gmail.com>
2024-12-07 01:24:27 -0500

    ringos: stub __stdout_write.

--- src/stdio/__stdout_write.c
+++ src/stdio/__stdout_write.c
@@ -1,8 +1,16 @@
 #include "stdio_impl.h"
 #include <sys/ioctl.h>
 
+// ringos
+#include <kernel/syscalls.h>
+
 size_t __stdout_write(FILE *f, const unsigned char *buf, size_t len)
 {
+	// ringos
+	ros_sys_debug_string(buf);
+
+	return;
+
 	struct winsize wsz;
 	f->write = __stdio_write;
 	if (!(f->flags & F_SVB) && __syscall(SYS_ioctl, f->fd, TIOCGWINSZ, &wsz))
